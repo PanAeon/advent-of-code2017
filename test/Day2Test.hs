32c1843f7a88a7c1ee3479de26583e03e5094993
PanAeon <vitalii.voloshyn@gmail.com>
2018-09-10 14:02:38 +0100

    This will definitely break in 2084 (TODO)

--- test/Day2Test.hs
+++ test/Day2Test.hs
@@ -11,19 +11,17 @@ divisibleTestCase = [r|
 3 8 6 5
 |]
 
+checksumTestCase = [r|
+5 1 9 5
+7 5 3
+2 4 6 8
+|]
+
 spec_day2 :: Spec
 spec_day2 = do
   describe "computeDivisibleChecksum" $ do
     it "shoud compute test case 1" $ do
       computeDivisibleChecksum (parse divisibleTestCase) `shouldBe` ( 9 :: Int)
-  -- describe "checksumShiftHalf" $ do
-  --   it "handles 1212" $ do
-  --     checksumShiftHalf "1212" `shouldBe` (6 :: Int)
-  --   it "handles 1221" $ do
-  --     checksumShiftHalf "1221" `shouldBe` (0 :: Int)
-  --   it "handles 123425" $ do
-  --     checksumShiftHalf "123425" `shouldBe` (4 :: Int)
-  --   it "handles 123123" $ do
-  --     checksumShiftHalf "123123" `shouldBe` (12 :: Int)
-  --   it "handles 12131415" $ do
-  --     checksumShiftHalf "12131415" `shouldBe` (4 :: Int)
+  describe "computeChecksum" $ do
+    it "should compute test case 1" $ do
+      computeChecksum (parse checksumTestCase) `shouldBe` (18 :: Int)
