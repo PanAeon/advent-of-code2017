282a6adf0bb19a0ec15eafdc3de27661fa9fc8b0
PanAeon <vitalii.voloshyn@gmail.com>
2018-11-05 21:17:06 +0000

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
+import           Day12
 import           Day13
-import Day14
-import Day12
-import Day16
+import           Day14
+import           Day16
 
 -- FIXME: write fast dfs, why it can't handle 16k nodes?
 main :: IO ()
-main = putStrLn $ show $ baz "nlciboghjmfdapek" task16Input
+main = putStrLn $ show $ iterateStrip 16 1000000000
 
   --putStrLn $ show $ calculateDelay
--- src/Day16.hs
+++ src/Day16.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase   #-}
+{-# LANGUAGE Strict       #-}
+{-# LANGUAGE StrictData   #-}
 module Day16
-    (baz, task16Input
+    ( task16Input, iterateStuff, nStep, iterateWhole, iterateStrip
     ) where
 
 
+-- FUCK, period is 126... uhu
 
-import           Data.Vector.Unboxed    (Vector)
-import qualified Data.Vector.Unboxed    as UV
+import           Data.Vector.Unboxed         (Vector)
+import qualified Data.Vector.Unboxed         as UV
 import           System.IO
 import           System.IO.Unsafe
 
@@ -14,20 +18,24 @@ import           System.IO.Unsafe
 import           Data.Char
 -- import           FunctionsAndTypesForParsing   (parseWithEof, parseWithLeftOver, regularParse)
 import           Data.List
-import           Text.Parsec.Char       (alphaNum, anyChar, char, digit, oneOf, satisfy, spaces, string)
-import           Text.Parsec.String     (Parser)
+import           Text.Parsec.Char            (alphaNum, anyChar, char, digit, oneOf, satisfy, spaces, string)
+import           Text.Parsec.String          (Parser)
 -- import           Text.Parsec.String.Char
 import           Control.Applicative
 import           Control.Monad
 import           Data.Bits
-import           Data.Map.Strict        (Map)
-import qualified Data.Map.Strict        as M
-import           Data.Maybe             (isJust)
-import           Data.Set               (Set)
-import qualified Data.Set               as S
-import qualified Numeric                as N
-import           Text.Parsec.Combinator (between, many1, sepBy)
-import           Text.Parsec.Prim       (parse, parseTest)
+import           Data.Map.Strict             (Map)
+import qualified Data.Map.Strict             as M
+import           Data.Maybe                  (isJust)
+import           Data.Set                    (Set)
+import qualified Data.Set                    as S
+-- import qualified Data.Vector.Algorithms.Insertion as IS
+import qualified Numeric                     as N
+import           Text.Parsec.Combinator      (between, many1, sepBy)
+import           Text.Parsec.Prim            (parse, parseTest)
+
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
 
 task16InputRaw = unsafePerformIO $ do
                 handle <- openFile "input/day16" ReadMode
@@ -36,9 +44,9 @@ task16InputRaw = unsafePerformIO $ do
 
 task16Input = parseLine task16InputRaw
 
-data Move = Spin Int
-          | Exchange Int Int
-          | Partner Char Char deriving (Show, Eq)
+data Move = Spin !Int
+          | Exchange !Int !Int
+          | Partner !Char !Char deriving (Show, Eq)
 
 parseLine :: String -> [Move]
 parseLine s = case (parse nodeP "" s) of
@@ -88,22 +96,26 @@ unpackLabels _N x = M.fromList zs
     zs = (\c -> (c, as !! (elemIndex' c bs))) <$> as
 
 -- blank -- pos0
+{-# INLINE swapLabels #-}
 swapLabels :: Int -> Int -> Move -> Int
-swapLabels _N x (Partner a b) = transform _N x (Exchange i j)
+swapLabels !_N !x !(Partner a b) = transform _N x (Exchange i j)
    where
      i = ord a - 97
      j = ord b - 97
 
+
+
 -- _N - 16
+{-# INLINE transform #-}
 transform :: Int -> Int -> Move -> Int
-transform _N x (Spin n) = (x' .|. x'') .&. ( (1 `shiftL` (_N * 4))- 1 )
+transform !_N !x !(Spin n) = (x' .|. x'') .&. ( (1 `shiftL` (_N * 4))- 1 )
   where
     r' = (n `mod` _N) * 4
     l' = (_N*4) - r'
     x'  =  x `rotateL` r'
     x'' =  x `rotateR` l'
 
-transform _N x (Exchange i j) = (x' .|. bitsI .|. bitsJ)  .&. ( (1 `shiftL` (_N * 4))- 1 )
+transform !_N !x !(Exchange i j) = (x' .|. bitsI .|. bitsJ)  .&. ( (1 `shiftL` (_N * 4))- 1 )
   where
     i'     = 4 * i
     j'     = 4 * j
@@ -166,6 +178,7 @@ FIXME: how to convert below nicely? bind + Reader?
 -}
 
 -- (transform _N)
+{-# INLINE dance #-}
 dance ::  Int -> (Int -> Move -> Int) -> [Move] -> Int
 dance a0 f xs =  foldl' f  a0 xs
 
@@ -183,19 +196,139 @@ wholeDance _N s moves = (s'', indexes, labels)
     s' = fromPos _N indexes
     s'' = (labelsMap M.!) <$> s'
 
-
-foo = "nlciboghjmfdapek"
-
-baz :: String -> [Move] -> String
-baz a0 ms =  last $ take 1000000000 $ tail $ iterate trans a0
+{-# INLINE stripDance #-}
+stripDance :: [Move] -> [Move] -> Int -> Int -> Int
+stripDance !movesA !movesB !_N !x0  = unpackLabels' _N indexes labels
   where
-    trans (a0:a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:a15:[]) =
-      [a12, a4, a2, a11, a14, a10, a6, a7, a3, a8, a15, a1, a9, a0, a5, a13]
+    p0 = pos0 .&. ( (1 `shiftL` (_N * 4))- 1 )
+    indexes = dance x0 (transform _N) movesA
+    labels  = dance p0 (swapLabels _N) movesB
+    -- labelsMap = unpackLabels _N labels
+    -- s' = fromPos _N indexes
+    -- s'' = (labelsMap M.!) <$> s'
+
+{-# INLINE stripper #-}
+stripper = stripDance (filter (not . isPartner) task16Input) (filter (isPartner) task16Input) 16
+
+unpackLabels' :: Int -> Int -> Int -> Int
+unpackLabels' _N tokens labels = bar as
+  where
+    idx i = (labels `rotateR` (4*i)) .&. 15
+    as = [ idx (tokens `rotateR` (4*i) .&. 15) | i <- [0..(_N - 1)]]
+    bar ::  [Int] -> Int
+    bar xs = foldl' f 0 [ (x, 4 * i)| (i,x) <- zip [0..(_N-1)] xs]
+      where
+        f x (i,p) = x .|. (i `rotateL` p)
+
+    --zs = (\c -> (c, as !! (elemIndex' c bs))) <$> as
+
+package :: String -> String -> String
+package indexes labels = xs
+  where
+    xs = (\x -> labels !! ((ord x) - 97)) <$> indexes
+-- as = take _N ['a'..'p']
+-- bs = fromPos _N x
+-- zs = (\c -> (c, as !! (elemIndex' c bs))) <$> as
+
+-- iterateLabels :: Int -> Int -> Int -> Int
+-- iterateLabels _N p x0 = last $ take 1000000000 $ tail $ iterate f x0
+--   where
+--     f x = undefined
+
+-- and "gbhcmpjikfealdno" is the wrong answer ...
+
+iterate' f x = x `seq` x : iterate' f (f x)
+-- iterate res, index:  oldhpjacfmkenbgi
+-- iterate res, label:  jdihakncopebflgm
+iterateStuff :: Int -> [Int] -> String -> String
+iterateStuff _M pattern x0 = UV.toList $ head $ drop _M  $ iterate' f (UV.fromList x0)
+   where
+     pattern' = UV.fromList pattern
+     f :: Vector Char -> Vector Char
+     f xs = UV.backpermute xs pattern'
+
+z = iterateStuff 2 (tranformX 5 143620) "abcde"
+
+
+
+secondStep nTimes = r
+  where
+    (res, idx, labelsI) = wholeDance 5 (take 5 ['a'..'p']) testInput
+    z = iterateStuff nTimes (tranformX 5 idx) "abcde"
+    l = iterateStuff nTimes (tranformX 5 labelsI) "abcde"
+    r = foo 5 (toPos 5 l) z
+
+
+iterateWhole _N nTimes = head $ drop nTimes  $ iterate' f (take _N ['a'..'p'])
+  where
+    -- _N = length input
+    f xs = let (res, _, _ ) = wholeDance _N (xs) task16Input
+           in res
+
+
+iterateStrip _N nTimes = fromPos _N $ head $ drop nTimes  $ iterate' stripper (toPos _N $ take _N ['a'..'p'])
+  -- where
+    -- f xs = stripDance _N (xs) task16Input
+
+
+
+-- wrong, compare with iterate strip
+nStep nTimes = r
+  where
+    (res, idx, labelsI) = wholeDance 16 (['a'..'p']) task16Input
+    z = iterateStuff nTimes (tranformX 16 idx) ['a'..'p']
+    l = iterateStuff nTimes (tranformX 16 labelsI) ['a'..'p']
+    r = foo 16 (toPos 16 l) z
+
+foo :: Int -> Int -> String -> String
+foo _N labels x = (labelsMap M.!) <$> x
+ where
+  labelsMap = unpackLabels _N labels
+
+       -- where
+         -- z = IS.sortBy (\a b -> compare (snd a) (snd b) ) (G.basicUnsafeThaw $ UV.zip xs pattern')
+         --UV.map fst  (IS.sortBy snd) _ -- (UV.zip xs pattern'))
+
+--
+-- baz :: String -> [Move] -> String
+-- baz a0 ms =  last $ take 1000000000 $ tail $ iterate trans a0
+--   where
+--     trans (a0:a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12:a13:a14:a15:[]) =
+--       [a12, a4, a2, a11, a14, a10, a6, a7, a3, a8, a15, a1, a9, a0, a5, a13]
     -- trans xs = fst <$> sortOn snd (zip xs [12,4,2,11,14,10,6,7,3,8,15,1,9,0,5,13])
 
 unsafeGet (Just x) = x
-tranformX :: [Int]
-tranformX = (\x -> unsafeGet $ elemIndex x "nlciboghjmfdapek") <$> ['a'..'p']
+tranformX :: Int -> Int -> [Int]
+tranformX _N i = (\x -> unsafeGet $ elemIndex x s) <$> (take _N ['a'..'p'])
+  where
+    s = fromPos _N i
+-- "jdihakncopebflgm"
 -- one but last
 --kicmdbghanejpflo -- it's not the right answer
 -- pdcjleghmaoinkbf wrong (it's off by 1)
+
+{-
+fromPos 16 4668291485119752150
+"gnhcliodpfkbjmae"
+fromPos 16 4668291485119752150
+"gnhcliodpfkbjmae"
+[14,11,3,7,15,9,0,2,5,12,10,4,13,1,6,8]
+
+then iterate names :
+[4,3,8,7,10,12,13,2,14,0,5,1,15,11,6,9]
+"jlhbakodcpenfgim"
+
+iterateStuff [4,3,8,7,10,12,13,2,14,0,5,1,15,11,6,9] ['a'..'p']
+-}
+
+-- whole dance, real output
+-- wholeDance 16 ['a'..'p'] task16Input
+-- ("nlciboghjmfdapek",4668291485119752150,-4006562156578334791)
+
+-- whole dance test output
+-- wholeDance 5 "abcde" testInput
+-- ("baedc",143620,78400)
+
+-- gbhcjaeimpfkldno wrong ..
+-- lmndopabcefghijk <- wrong!! .. again
+-- inbdjaehmpfkgloc wrong
